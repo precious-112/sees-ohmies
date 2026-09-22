f56dc2ae0ac65e6bfad3a5b7b0157703438d4bf3
Precious Oluwatosin Igbayisemore <edithmore200@gmail.com>
2026-09-22 23:03:05 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase, ref, push, onChildAdded } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
